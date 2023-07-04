ca5519e5b297011ddf78ab5afb9888e347b5b3fc
Александр Минаев <a_minaev14@mail.ru>
2023-07-04 23:44:25 +0300

    Create Users models

--- morec/users/models.py
+++ morec/users/models.py
@@ -1,3 +1,18 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+
+class User(AbstractUser):
+    """Расширенная модель для пользователей."""
+    email = models.EmailField('Почта', max_length=254, unique=True)
+    username = models.SlugField('Логин', max_length=150, unique=True,)
+    first_name = models.CharField('Имя', max_length=150)
+    last_name = models.CharField('Фамилия', max_length=150)
+
+    def __str__(self):
+        return self.username
+
+    class Meta:
+        ordering = ['-pk']
+        verbose_name = 'Пользователь'
+        verbose_name_plural = 'Пользователи'
