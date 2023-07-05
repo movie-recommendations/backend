b1c3389da2215f58034535ee405253952ae20771
Александр Минаев <a_minaev14@mail.ru>
2023-07-05 19:41:29 +0300

    Created a field 'birth_date' in the model 'User'

--- morec/users/models.py
+++ morec/users/models.py
@@ -4,10 +4,16 @@ from django.db import models
 
 class User(AbstractUser):
     """Расширенная модель для пользователей."""
-    email = models.EmailField('Почта', max_length=254, unique=True)
-    username = models.SlugField('Логин', max_length=150, unique=True,)
-    first_name = models.CharField('Имя', max_length=150)
-    last_name = models.CharField('Фамилия', max_length=150)
+    email = models.EmailField('Почта', max_length=254, unique=True,
+                              help_text='Введите свой Email')
+    username = models.SlugField('Логин', max_length=150, unique=True,
+                                help_text='Введите свой логин')
+    first_name = models.CharField('Имя', max_length=150,
+                                  help_text='Введите своё имя')
+    last_name = models.CharField('Фамилия', max_length=150,
+                                 help_text='Введите свою фамилию')
+    birth_date = models.DateField('Дата рождения', null=True, blank=True,
+                                  help_text='Введите свою дату рождения')
 
     def __str__(self):
         return self.username
