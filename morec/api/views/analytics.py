fcd34c3a86a0b216d7e48905f14b07f6ce5cca1c
Александр Ромашков <eho231@mail.ru>
2023-10-12 01:35:31 +0500

    Add analytics

--- morec/api/views/analytics.py
+++ morec/api/views/analytics.py
@@ -1,5 +1,5 @@
 from rest_framework.decorators import api_view, permission_classes
-from rest_framework.permissions import AllowAny, IsAuthenticated
+from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 
 from analytics.model.inference import get_inference, user_processing
